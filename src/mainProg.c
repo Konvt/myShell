c84e73f3121cd2a27ddd476987c9ccf1cd0e40eb
Konvt <iftiy@foxmail.com>
2023-08-11 02:18:02 +0800

    update

--- src/mainProg.c
+++ src/mainProg.c
@@ -14,33 +14,28 @@ int main(int argc, char **argv)
 
     mgr.Login(&mgr);
     int cwdLen = strlen(mgr.cwd), promptLen = basicLen+cwdLen;
-    char *prompt = (char*)malloc(sizeof(char)*(promptLen+1));
-    while (1) {
-        /* prompt will be re-spliced only after the cwd has been updated */
-        if (MatchSubstr(prompt, mgr.cwd, 0) == FAILED) {
-            memset(prompt, BLANK, sizeof(char)*(promptLen+1));
-            sprintf(prompt, format, mgr.hostName, mgr.uid, mgr.cwd); // splicing prompt
-        }
+    char *prompt = (char*)calloc(promptLen+1, sizeof(char));
+    sprintf(prompt, format, mgr.hostName, mgr.uid, mgr.cwd); // splicing prompt
 
+    while (1) {
         mgr.cmdMgr.Scanner(&mgr.cmdMgr, stdin, prompt)->Paraser(&mgr.cmdMgr, " ");
         mgr.lastCmdType = mgr.cmdMgr.Processor(&mgr.cmdMgr);
         if (mgr.lastCmdType == buildinCd) {
             mgr.Update(&mgr);
             cwdLen = strlen(mgr.cwd);
-        }
-
-        /* ensure that the cwd can always be loaded into prompt */
-        if (promptLen < basicLen+cwdLen) {
-            /* if it is too small, an array expansion will be triggered
-             * and make it as big as possible */
-            promptLen = basicLen+(cwdLen*2);
-            free(prompt);
-            prompt = (char*)malloc(sizeof(char)*(promptLen+1));
+            /* ensure that the cwd can always be loaded into prompt */
+            if (promptLen < basicLen+cwdLen) {
+                /* if it is too small, an array expansion will be triggered
+                * and make it as big as possible */
+                promptLen = basicLen+(cwdLen*2);
+                free(prompt);
+                prompt = (char*)calloc(promptLen+1, sizeof(char));
+            }
+            sprintf(prompt, format, mgr.hostName, mgr.uid, mgr.cwd); // splicing prompt
         }
     }
 
-    /* i don't think this part is usually useful
-     * unless you're using it in an embedded system */
+    /* i don't think this part is usually useful */
     free(prompt); mgr.Destructor(&mgr);
     /* anyways, be happy */
 
